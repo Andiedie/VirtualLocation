a8dfc772e30fa600ccf7a169378cb4a38f4e5762
andie <zchangan@163.com>
2024-09-25 20:12:39 +0800

    Initial Commit

---
+++ VirtualLocation/VirtualLocationApp.swift
@@ -0,0 +1,17 @@
+//
+//  VirtualLocationApp.swift
+//  VirtualLocation
+//
+//  Created by Andie on 2024/9/25.
+//
+
+import SwiftUI
+
+@main
+struct VirtualLocationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ VirtualLocationTests/VirtualLocationTests.swift
@@ -0,0 +1,17 @@
+//
+//  VirtualLocationTests.swift
+//  VirtualLocationTests
+//
+//  Created by Andie on 2024/9/25.
+//
+
+import Testing
+@testable import VirtualLocation
+
+struct VirtualLocationTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
